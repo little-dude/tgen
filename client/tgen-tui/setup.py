bd689ce544c0806d7ffd16a0fe430df0d5255e7b
Corentin Henry <corentinhenry@gmail.com>
2016-05-24 15:13:56 +0200

    add python package for future TUI client

---
+++ client/tgen-tui/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='tgen_tui',
+    version='0.1',
+    include_package_data=True,
+    description='a basic TUI for tgen',
+    url='http://github.com/corentih/tgen',
+    author='Corentin Henry',
+    author_email='corentin.henry@nokia.com',
+    license='MIT',
+    packages=['tui'],
+    install_requires=[line for line in open('requirements.txt')],
+    entry_points={'console_scripts': ['tgen-tui = tui:run']}
+)
