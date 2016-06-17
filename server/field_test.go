d40a353a53cf6107dca5ee8a707c49213d60c72e
Corentin Henry <corentinhenry@gmail.com>
2016-06-17 17:03:32 +0200

    server: fix unit tests

--- server/field_test.go
+++ server/field_test.go
@@ -13,42 +13,42 @@ type testField8 struct {
 
 var tests8 = []testField8{
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 0, count: 1, mode: INCREMENT, mask: 0, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 0, Count: 1, Mode: INCREMENT, Mask: 0, FullMask: 255},
 		index:    []uint{0, 1},
 		expected: []uint8{0, 0},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 1, count: 1, mode: INCREMENT, mask: 255, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 1, Count: 1, Mode: INCREMENT, Mask: 255, FullMask: 255},
 		index:    []uint{0, 1},
 		expected: []uint8{0, 0},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 0, count: 2, mode: INCREMENT, mask: 255, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 0, Count: 2, Mode: INCREMENT, Mask: 255, FullMask: 255},
 		index:    []uint{0, 1, 255},
 		expected: []uint8{0, 0, 0},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 1, count: 2, mode: INCREMENT, mask: 255, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 1, Count: 2, Mode: INCREMENT, Mask: 255, FullMask: 255},
 		index:    []uint{0, 1, 2},
 		expected: []uint8{0, 1, 0},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 1, count: 255, mode: INCREMENT, mask: 255, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 1, Count: 255, Mode: INCREMENT, Mask: 255, FullMask: 255},
 		index:    []uint{255, 256},
 		expected: []uint8{0, 1},
 	},
 	{
-		field:    Field8{firstValue: 255, value: 255, step: 1, count: 255, mode: INCREMENT, mask: 0, fullMask: 255},
+		field:    Field8{FirstValue: 255, Value: 255, Step: 1, Count: 255, Mode: INCREMENT, Mask: 0, FullMask: 255},
 		index:    []uint{0, 1, 2},
 		expected: []uint8{255, 255, 255},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 255, count: 255, mode: INCREMENT, mask: 64, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 255, Count: 255, Mode: INCREMENT, Mask: 64, FullMask: 255},
 		index:    []uint{0, 1, 2},
 		expected: []uint8{0, 64, 0},
 	},
 	{
-		field:    Field8{firstValue: 0, value: 0, step: 32, count: 255, mode: INCREMENT, mask: 255, fullMask: 255},
+		field:    Field8{FirstValue: 0, Value: 0, Step: 32, Count: 255, Mode: INCREMENT, Mask: 255, FullMask: 255},
 		index:    []uint{0, 1, 2, 3, 4, 5, 6, 7, 8},
 		expected: []uint8{0, 32, 64, 96, 128, 160, 192, 224, 0},
 	},
@@ -60,7 +60,7 @@ func TestField8Increment(t *testing.T) {
 		values = make([]uint8, len(test.expected))
 		for i, index := range test.index {
 			test.field.SetCurrentValue(index)
-			values[i] = test.field.value
+			values[i] = test.field.Value
 		}
 		if reflect.DeepEqual(values, test.expected) == false {
 			t.Error(
@@ -74,7 +74,7 @@ func TestField8Increment(t *testing.T) {
 
 func TestField8Getters(t *testing.T) {
 	var field *Field8
-	field = &Field8{firstValue: 0, value: 0, step: 32, count: 255, mode: INCREMENT, mask: 255, fullMask: 255}
+	field = &Field8{FirstValue: 0, Value: 0, Step: 32, Count: 255, Mode: INCREMENT, Mask: 255, FullMask: 255}
 	if !reflect.DeepEqual(field.GetMask(), []byte{255}) {
 		t.Error(field.GetMask())
 	}
@@ -88,28 +88,28 @@ func TestField8Getters(t *testing.T) {
 
 func TestField8Setters(t *testing.T) {
 	var field *Field8
-	field = &Field8{fullMask: 255}
+	field = &Field8{FullMask: 255}
 
 	field.SetValue([]byte{255})
-	if !reflect.DeepEqual(field.value, uint8(255)) {
-		t.Error(field.value)
+	if !reflect.DeepEqual(field.Value, uint8(255)) {
+		t.Error(field.Value)
 	}
 
 	field.SetMask([]byte{32})
-	if !reflect.DeepEqual(field.mask, uint8(32)) {
-		t.Error(field.mask)
+	if !reflect.DeepEqual(field.Mask, uint8(32)) {
+		t.Error(field.Mask)
 	}
 
 	field.SetStep([]byte{0})
-	if !reflect.DeepEqual(field.step, uint8(0)) {
-		t.Error(field.step)
+	if !reflect.DeepEqual(field.Step, uint8(0)) {
+		t.Error(field.Step)
 	}
 }
 
 func TestField16Getters(t *testing.T) {
 	var field *Field16
 
-	field = &Field16{firstValue: 0, value: 0, step: 32, count: 255, mode: INCREMENT, mask: 255, fullMask: 65535}
+	field = &Field16{FirstValue: 0, Value: 0, Step: 32, Count: 255, Mode: INCREMENT, Mask: 255, FullMask: 65535}
 	if !reflect.DeepEqual(field.GetMask(), []byte{0, 255}) {
 		t.Error(field.GetMask())
 	}
@@ -120,7 +120,7 @@ func TestField16Getters(t *testing.T) {
 		t.Error(field.GetValue())
 	}
 
-	field = &Field16{firstValue: 0, value: 65535, step: 256, count: 1025, mode: INCREMENT, mask: 1025, fullMask: 65535}
+	field = &Field16{FirstValue: 0, Value: 65535, Step: 256, Count: 1025, Mode: INCREMENT, Mask: 1025, FullMask: 65535}
 	if !reflect.DeepEqual(field.GetMask(), []byte{4, 1}) {
 		t.Error(field.GetMask())
 	}
@@ -134,49 +134,49 @@ func TestField16Getters(t *testing.T) {
 
 func TestField16Setters(t *testing.T) {
 	var field *Field16
-	field = &Field16{fullMask: 65535}
+	field = &Field16{FullMask: 65535}
 
 	field.SetValue([]byte{0, 255})
-	if !reflect.DeepEqual(field.value, uint16(255)) {
-		t.Error(field.value)
+	if !reflect.DeepEqual(field.Value, uint16(255)) {
+		t.Error(field.Value)
 	}
 	field.SetValue([]byte{255, 255})
-	if !reflect.DeepEqual(field.value, uint16(65535)) {
-		t.Error(field.value)
+	if !reflect.DeepEqual(field.Value, uint16(65535)) {
+		t.Error(field.Value)
 	}
 
 	field.SetMask([]byte{0, 32})
-	if !reflect.DeepEqual(field.mask, uint16(32)) {
-		t.Error(field.mask)
+	if !reflect.DeepEqual(field.Mask, uint16(32)) {
+		t.Error(field.Mask)
 	}
 	field.SetMask([]byte{1, 0})
-	if !reflect.DeepEqual(field.mask, uint16(256)) {
-		t.Error(field.mask)
+	if !reflect.DeepEqual(field.Mask, uint16(256)) {
+		t.Error(field.Mask)
 	}
 
 	field.SetStep([]byte{0, 0})
-	if !reflect.DeepEqual(field.step, uint16(0)) {
-		t.Error(field.step)
+	if !reflect.DeepEqual(field.Step, uint16(0)) {
+		t.Error(field.Step)
 	}
 	field.SetStep([]byte{4, 1})
-	if !reflect.DeepEqual(field.step, uint16(1025)) {
-		t.Error(field.step)
+	if !reflect.DeepEqual(field.Step, uint16(1025)) {
+		t.Error(field.Step)
 	}
 
-	field = &Field16{fullMask: 0x01ff}
+	field = &Field16{FullMask: 0x01ff}
 
 	field.SetValue([]byte{255, 255})
-	if !reflect.DeepEqual(field.value, uint16(0x01ff)) {
-		t.Error(field.value)
+	if !reflect.DeepEqual(field.Value, uint16(0x01ff)) {
+		t.Error(field.Value)
 	}
 
 	field.SetMask([]byte{1, 0})
-	if !reflect.DeepEqual(field.mask, uint16(0x0100)) {
-		t.Error(field.mask)
+	if !reflect.DeepEqual(field.Mask, uint16(0x0100)) {
+		t.Error(field.Mask)
 	}
 	field.SetStep([]byte{4, 1})
-	if !reflect.DeepEqual(field.step, uint16(0x0001)) {
-		t.Error(field.step)
+	if !reflect.DeepEqual(field.Step, uint16(0x0001)) {
+		t.Error(field.Step)
 	}
 }
 
@@ -189,65 +189,65 @@ type testLongField struct {
 var testsLong = []testLongField{
 	{
 		field: LongField{
-			firstValue: []byte{0, 0, 0, 0, 0, 0},
-			value:      []byte{0, 0, 0, 0, 0, 0},
-			step:       []byte{0, 0, 0, 0, 0, 0},
-			count:      1,
-			mode:       INCREMENT,
-			mask:       []byte{255, 255, 255, 255, 255, 255},
-			fullMask:   []byte{255, 255, 255, 255, 255, 255},
+			FirstValue: []byte{0, 0, 0, 0, 0, 0},
+			Value:      []byte{0, 0, 0, 0, 0, 0},
+			Step:       []byte{0, 0, 0, 0, 0, 0},
+			Count:      1,
+			Mode:       INCREMENT,
+			Mask:       []byte{255, 255, 255, 255, 255, 255},
+			FullMask:   []byte{255, 255, 255, 255, 255, 255},
 		},
 		index:    []uint{0, 1},
 		expected: [][]byte{{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
 	},
 	{
 		field: LongField{
-			firstValue: []byte{0, 0, 0, 0, 0, 0},
-			value:      []byte{0, 0, 0, 0, 0, 0},
-			step:       []byte{1, 1, 1, 1, 1, 1},
-			count:      2,
-			mode:       INCREMENT,
-			mask:       []byte{255, 255, 255, 255, 255, 255},
-			fullMask:   []byte{255, 255, 255, 255, 255, 255},
+			FirstValue: []byte{0, 0, 0, 0, 0, 0},
+			Value:      []byte{0, 0, 0, 0, 0, 0},
+			Step:       []byte{1, 1, 1, 1, 1, 1},
+			Count:      2,
+			Mode:       INCREMENT,
+			Mask:       []byte{255, 255, 255, 255, 255, 255},
+			FullMask:   []byte{255, 255, 255, 255, 255, 255},
 		},
 		index:    []uint{0, 1},
 		expected: [][]byte{{0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1}},
 	},
 	{
 		field: LongField{
-			firstValue: []byte{0, 0, 0, 0, 0, 0},
-			value:      []byte{0, 0, 0, 255, 0, 0},
-			step:       []byte{0, 0, 0, 1, 0, 0},
-			count:      2,
-			mode:       INCREMENT,
-			mask:       []byte{255, 255, 255, 255, 255, 255},
-			fullMask:   []byte{255, 255, 255, 255, 255, 255},
+			FirstValue: []byte{0, 0, 0, 0, 0, 0},
+			Value:      []byte{0, 0, 0, 255, 0, 0},
+			Step:       []byte{0, 0, 0, 1, 0, 0},
+			Count:      2,
+			Mode:       INCREMENT,
+			Mask:       []byte{255, 255, 255, 255, 255, 255},
+			FullMask:   []byte{255, 255, 255, 255, 255, 255},
 		},
 		index:    []uint{1},
 		expected: [][]byte{{0, 0, 1, 0, 0, 0}},
 	},
 	{
 		field: LongField{
-			firstValue: []byte{0, 0, 0, 0, 0, 0},
-			value:      []byte{0, 0, 0, 255, 0, 0},
-			step:       []byte{0, 0, 0, 1, 0, 0},
-			count:      2,
-			mode:       INCREMENT,
-			mask:       []byte{255, 255, 0, 255, 255, 255},
-			fullMask:   []byte{255, 255, 255, 255, 255, 255},
+			FirstValue: []byte{0, 0, 0, 0, 0, 0},
+			Value:      []byte{0, 0, 0, 255, 0, 0},
+			Step:       []byte{0, 0, 0, 1, 0, 0},
+			Count:      2,
+			Mode:       INCREMENT,
+			Mask:       []byte{255, 255, 0, 255, 255, 255},
+			FullMask:   []byte{255, 255, 255, 255, 255, 255},
 		},
 		index:    []uint{1},
 		expected: [][]byte{{0, 0, 0, 0, 0, 0}},
 	},
 	{
 		field: LongField{
-			firstValue: []byte{1, 2, 3, 4, 5, 6},
-			value:      []byte{255, 255, 255, 255, 255, 255},
-			step:       []byte{0, 0, 0, 0, 0, 255},
-			count:      1000,
-			mode:       INCREMENT,
-			mask:       []byte{255, 255, 255, 255, 255, 255},
-			fullMask:   []byte{255, 255, 255, 255, 255, 255},
+			FirstValue: []byte{1, 2, 3, 4, 5, 6},
+			Value:      []byte{255, 255, 255, 255, 255, 255},
+			Step:       []byte{0, 0, 0, 0, 0, 255},
+			Count:      1000,
+			Mode:       INCREMENT,
+			Mask:       []byte{255, 255, 255, 255, 255, 255},
+			FullMask:   []byte{255, 255, 255, 255, 255, 255},
 		},
 		index:    []uint{999, 1000},
 		expected: [][]byte{{0, 0, 0, 0, 0, 254}, {1, 2, 3, 4, 5, 6}},
@@ -260,8 +260,8 @@ func TestLongField(t *testing.T) {
 		values = make([][]byte, len(test.expected))
 		for i, index := range test.index {
 			test.field.SetCurrentValue(index)
-			values[i] = make([]byte, len(test.field.value))
-			copy(values[i], test.field.value)
+			values[i] = make([]byte, len(test.field.Value))
+			copy(values[i], test.field.Value)
 		}
 		if reflect.DeepEqual(values, test.expected) == false {
 			t.Error(
