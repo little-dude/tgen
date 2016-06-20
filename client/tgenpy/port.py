46911b012630a32d12ca00aca8b14a778924d77c
Corentin Henry <corentinhenry@gmail.com>
2016-06-20 16:42:26 +0200

    client: add capture

--- client/tgenpy/port.py
+++ client/tgenpy/port.py
@@ -16,6 +16,18 @@ class Port(object):
     def start_send(self, stream_ids):
         self._capnp_port.startSend(stream_ids).wait()
 
-    def wait_send(self, timeout=1):
+    def wait_send(self, timeout=0):
         res = self._capnp_port.waitSend(timeout).wait()
         return (res.done, res.error)
+
+    def start_capture(self, savePath, snapshot_length=9999, promiscuous=True,
+                      packet_count=0, timeout=0):
+        self._capnp_port.startCapture(savePath, snapshot_length, timeout,
+                                      packet_count, promiscuous).wait()
+
+    def wait_capture(self, timeout=0):
+        res = self._capnp_port.waitCapture(timeout).wait()
+        return (res.done, res.error)
+
+    def stop_capture(self):
+        self._capnp_port.stopCapture().wait()
